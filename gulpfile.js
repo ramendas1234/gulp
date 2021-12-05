9a156d04cde1ed3c7a7c4e92e8867545b012bb60
Bappa Dey <bappa.dey@einzigtech.com>
2021-12-05 20:19:01 +0530

    how to use auto prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require( 'gulp' );
 var rename = require( 'gulp-rename' );
 var sass = require('gulp-sass')(require('sass'));
+var autoprefixer = require( 'gulp-autoprefixer' );
 
 var styleSRC = './src/scss/style.scss';
 var styleDIST = './dist/css/'
@@ -11,6 +12,10 @@ gulp.task('style', function(){
     .pipe( sass( { errorLogToConsole: true, outputStyle: 'compressed' } ) )
     //.pipe( sass.sync().on('error', sass.logError) )
     .on( 'error', console.log.bind( console ) )
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe( rename( { suffix : '.min' } ) )
     .pipe( gulp.dest( styleDIST ) )
 });
