1d4ebfaaac5ca3e47cd60de23c0f7acb4b078a03
Bappa Dey <bappa.dey@einzigtech.com>
2021-12-05 20:02:38 +0530

    how to compile and minfy auto sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require( 'gulp' );
 var rename = require( 'gulp-rename' );
+var sass = require('gulp-sass')(require('sass'));
 
-var styleSRC = './src/scss/style.css';
+var styleSRC = './src/scss/style.scss';
 var styleDIST = './dist/css/'
 
-gulp.task('styles', function(){
+gulp.task('style', function(){
     gulp.src( styleSRC )
-    .pipe( rename( { suffix : 'min' } ) )
+    //sass.sync().on('error', sass.logError)
+    .pipe( sass( { errorLogToConsole: true, outputStyle: 'compressed' } ) )
+    //.pipe( sass.sync().on('error', sass.logError) )
+    .on( 'error', console.log.bind( console ) )
+    .pipe( rename( { suffix : '.min' } ) )
     .pipe( gulp.dest( styleDIST ) )
 });
